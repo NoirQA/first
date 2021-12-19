e7b198a7fbcae891579dc3f6b370ce735a3e4738
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-20 02:54:37 +0300

    add hw1 string
    часть домашней работы

--- Python_HW_1.py
+++ Python_HW_1.py
@@ -1,36 +1,41 @@
 # Переменная String
 myName = 'Eugne'
-print(type(myName),myName)
+print(('Тип переменой: '),type(myName),myName)
 
 # Переменная Integer
 myAge = 38
-print(type(myAge),myAge)
+print(('Тип переменой: '),type(myAge),myAge)
 
 # Переменная Float
 myTemp = 36.6
-print(type(myTemp),myTemp)
+print(('Тип переменой: '),type(myTemp),myTemp)
 
 # Переменная Bytes
 myAgeBytes = bytes(38)
-print(type(myAgeBytes),myAgeBytes)
+print(('Тип переменой: '),type(myAgeBytes),myAgeBytes)
 
 # Переменная Tuple (Нельзя добавить элемент, нельзя удалить элемент, )
 myFamaly = ("Olga", "Plato", "Sofi")
-print(type(myFamaly),myFamaly)
+print(('Тип переменой: '),type(myFamaly),myFamaly)
 
 # Переменная List
 myDocList = ["Passport", "Driver's license", "Deditation insurance"]
-print(type(myDocList), myDocList)
+print(('Тип переменой: '),type(myDocList), myDocList)
 
 # Переменная set (элементы множества всегда уникальны, неупорядоченная коллекция. Для создания пустого множества нужно непосредственно использовать set())
 mySets = {1, 1, 2, 3, 3, 2}
-print(type(mySets),mySets)
+print(('Тип переменой: '),type(mySets),mySets)
 
 #   Переменная frozenset //
 myLib = frozenset('П у ш к и')
-print(type(myLib),myLib)
+print(('Тип переменой: '),type(myLib),myLib)
 
 # Переменная Dict (Словать)
 
 myDict = {'hello':'Привет', 'bye':'Пока'}
-print(type(myDict),myDict)
+print(('Тип переменой: '),type(myDict),myDict)
+
+strTextOne = 'Это '
+strTextTwo = 'Восхитительно'
+strTextThree = strTextOne + strTextTwo
+print('Pythe',(strTextThree))

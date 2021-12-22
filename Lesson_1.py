77fdf132492d9514313ce3fbdf11107bea8c46f1
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-22 14:18:43 +0300

    123

---
+++ Lesson_1.py
@@ -0,0 +1,19 @@
+# print('Hello_Python_QA')
+#
+# print('Hello', 'Python', 'QA')
+#
+# print('Hello', 'Python', 'QA', sep='@')
+#
+# print('Hello '+'Python '+'QA')
+#
+# name = input('Input our name: ')
+# print('Hello, ', name)
+name = 'Platon'
+len_name = len(name)
+sorted_name = sorted(name)
+name_type = type(name)
+
+print('Name:', name)
+print('Len:', len_name)
+print('Sorted_name:',sorted_name)
+print('Name type:',name_type)

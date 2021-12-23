933dd49d720089c0b5c1f9f709808ddade566e51
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-23 15:18:51 +0300

    Python_2 Arithmetic operations

---
+++ Python_2_Arithmetic operations.py
@@ -0,0 +1,16 @@
+import math
+
+a = 2
+b = 5
+# result = b % a
+# print('Result=', a % b) # не по феншую лучше передать на вывод в переменной
+# if result == 0:
+#     print('Event')
+# else:
+#     print('odd')
+b += 20 + 100
+
+print('Result=',b)
+
+# print('Result=', result, type(result)) # Так норм, но работает и так и так !
+

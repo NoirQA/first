2db91c8cb632f717c765a24ab93f87358a9d7fdf
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-24 13:29:41 +0300

    HW_2 + boolean

---
+++ boolean.py
@@ -0,0 +1,17 @@
+a = True
+b = False
+
+age = 32
+weight = 97
+salary = 1000
+
+z = age == 323 or weight == 98 or salary == 11000
+
+print('Z_result =', z)
+
+if age == 42:
+    print('True --', True)
+elif weight == 97:
+    print(weight)
+else:
+    print('ELSE')

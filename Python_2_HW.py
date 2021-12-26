7719f1ab6cf8b17bf39c101e7bf24401792d8990
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-27 00:12:40 +0300

    Python_2 Arithmetic operations

---
+++ Python_2_HW.py
@@ -0,0 +1,14 @@
+item_1 = 23
+item_2 = 73
+result_sum = item_1 + item_2
+print('result_sum =', result_sum)
+
+result_subtr = item_2 - item_1
+print('result_subtr =', result_subtr)
+
+result_multi = item_1 * item_2
+print('result_multi =', result_multi)
+
+result_exp = item_1 ** item_2
+print('result_exp =', result_exp)
+

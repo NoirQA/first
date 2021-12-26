e5223846628494746f97cd1659ff8bb4f05ebae8
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-27 02:30:38 +0300

    Python_2 Arithmetic operations на сегодня все

--- Python_2_HW.py
+++ Python_2_HW.py
@@ -1,3 +1,5 @@
+import math
+
 item_1 = 23
 item_2 = 73
 result_sum = item_1 + item_2
@@ -12,3 +14,21 @@ print('result_multi =', result_multi)
 result_exp = item_1 ** item_2
 print('result_exp =', result_exp)
 
+result_m_exp = math.pow(item_1, item_2)
+print('result_m_exp =', result_m_exp)
+
+result_s_root = item_2 ** (0.5)
+print('Квадратный корень из result_s_root ' + str(item_2) + ' это ' + str(result_s_root))
+
+result_s_m_root = math.sqrt(item_2)
+print('Квадратный корень из result_s_m_root ' + str(item_2) + ' это ' + str(result_s_m_root), 'math.sqrt')
+
+# 17. Создать переменную result_mp_s_root в которой вы найдёте квадратный корень любой из переменной item используя
+# библиотеку math используя метод pow.
+#  Или я чего то не понял или это задание выполнено в строке 17 (поправьте если я ошибаюсь)
+
+item_1 = 93 # Odd
+item_2 = 20 # Even
+
+result_division = item_1 / item_2
+print('result_division = ', result_division)

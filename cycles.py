3cf3a4c7b1e2a124ea0877fbd12b9eb4b592475b
NoirQA <94997768+NoirQA@users.noreply.github.com>
2021-12-29 22:52:31 +0300

    Python_3 cycle end

--- cycles.py
+++ cycles.py
@@ -1,18 +1,34 @@
 import time
 
-count = 0
-while count < 1:
-    count += 1
-    print(count,'-- Hello')
-    time.sleep(.500)
+curr = 'USD'
+cuss_rate = 76.54
 
-arr = [4,23,553,20,2324,50,23,90,23,1000,23]
+while True:
 
-for i in arr:
-    dev_i = i % 10
-    if dev_i == 0:
-        print('i = ', i, 'dev_i', dev_i)
-    # multiply_i = i * 10
-    # print('Nubers',multiply_i)
-    time.sleep(.500)
+    usd_input = input('Enter USD: ')
+    usd_rub = float(usd_input) * cuss_rate
+
+    print('You enter USD:',usd_input)
+    print('Your RUS:',usd_rub)
+
+
+
+
+# count = 0
+# while count < 1:
+#     count += 1
+#     print(count,'-- Hello')
+#     time.sleep(.500)
+#
+# arr = [4,23,553,20,2324,50,23,90,23,1000,23]
+#
+# for i in arr:
+#     print('i =', i)
+#     if i == 23:
+#         continue
+#     print('Hello', i * 10)
+#     print('--------------')
+#     # multiply_i = i * 10
+#     # print('Nubers',multiply_i)
+#     time.sleep(.500)
 
